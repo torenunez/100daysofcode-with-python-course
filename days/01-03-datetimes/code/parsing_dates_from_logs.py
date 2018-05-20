1cdef8f017999e70db00ab4a0b9e5eac4314a402
torenunez <salvador.nunez@berkeley.edu>
2018-05-19 23:21:55 -0700

    day 2 dateime exercise

---
+++ days/01-03-datetimes/code/parsing_dates_from_logs.py
@@ -0,0 +1,48 @@
+'''Extract datetimes from log entries and calculate the time
+   between the first and last shutdown events'''
+from datetime import datetime
+import os
+import urllib.request
+
+SHUTDOWN_EVENT = 'Shutdown initiated'
+
+# prep: read in the logfile
+logfile = os.path.join('/tmp', 'log')
+urllib.request.urlretrieve('http://bit.ly/2AKSIbf', logfile)
+
+with open(logfile) as f:
+    loglines = f.readlines()
+
+
+# for you to code:
+
+def convert_to_datetime(line):
+    '''TODO 1:
+       Given a log line extract its timestamp and convert it to a datetime object.
+       For example calling the function with:
+       INFO 2014-07-03T23:27:51 supybot Shutdown complete.
+       returns:
+       datetime(2014, 7, 3, 23, 27, 51)'''
+    second_string = line.split()[1]
+    dt_format = '%Y-%m-%dT%H:%M:%S'
+    second_dt =  datetime.strptime(second_string, dt_format)
+    return second_dt
+
+
+def time_between_shutdowns(loglines):
+    '''TODO 2:
+       Extract shutdown events ("Shutdown initiated") from loglines and calculate the
+       timedelta between the first and last one.
+       Return this datetime.timedelta object.'''
+
+    shutdown_datetimes = []
+
+    for line in loglines:
+        if 'Shutdown initiated' in line:
+            s_dt = convert_to_datetime(line)
+            shutdown_datetimes.append(s_dt)
+
+    shutdown_delta = max(shutdown_datetimes) - min(shutdown_datetimes)
+
+    return shutdown_delta
+
